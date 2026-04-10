33c5a5c21423212ed5af680611b4352fdf3db673
Ashnoor Wadhera <ashnoorwadhera@Ashnoors-MacBook-Air.local>
2026-04-10 16:57:56 +0530

    ashnoor

--- Simulator.py
+++ Simulator.py
@@ -1,1 +1,23 @@
+import sys
 
+MASK32 = 0xFFFFFFFF
+
+DATA_MEM_BASE = 0x00010000
+DATA_MEM_WORDS = 32
+DATA_MEM_END = DATA_MEM_BASE + 4 * DATA_MEM_WORDS - 4
+
+STACK_BASE = 0x00000100
+STACK_END = 0x0000017C
+
+
+class SimulationError(Exception):
+    pass
+
+
+def u32(x):
+    return x & MASK32
+
+
+def s32(x):
+    x &= MASK32
+    return x if x < (1 << 31) else x - (1 << 32)
